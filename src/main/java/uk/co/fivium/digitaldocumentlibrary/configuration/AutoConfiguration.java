3ab4568faee067ee452b53501297a13b6172bab0
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2024-08-07 17:34:17 +0100

    use the slf4j adapter (#29)

--- src/main/java/uk/co/fivium/digitaldocumentlibrary/configuration/AutoConfiguration.java
+++ src/main/java/uk/co/fivium/digitaldocumentlibrary/configuration/AutoConfiguration.java
@@ -1,5 +1,7 @@
 package uk.co.fivium.digitaldocumentlibrary.configuration;
 
+import com.openhtmltopdf.slf4j.Slf4jLogger;
+import com.openhtmltopdf.util.XRLog;
 import java.time.Clock;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan("uk.co.fivium.digitaldocumentlibrary")
 @EnableScheduling
 class AutoConfiguration {
+
+  AutoConfiguration() {
+    // https://github.com/danfickle/openhtmltopdf/wiki/Logging#slf4j-logging-adapter
+    XRLog.setLoggerImpl(new Slf4jLogger());
+  }
 
   @Bean
   @ConditionalOnMissingBean
